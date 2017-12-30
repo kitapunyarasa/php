a153b26c6a61dc1fdbe072dccaf1c7ddd88437bd
kitapunyarasa <kitapunyarasa@gmail.com>
2017-12-30 17:43:10 +0700

    cobain aja

---
+++ test.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$input = "aku suka kamu"; //.masukkan kata atau kalimat yang ingin dibalik
+//.mulai memproses input
+$count = strlen($input); //.menghitung jumlah karakter
+echo "<b>input = </b>" . $input; //.menampilkan input
+echo "<hr />"; //.menampilkan pembatas antara input dan output
+echo "<b>output = </b>"; //.menampilkan output
+for ($i = $count; $i >= 0; $i--) {
+    $output = substr($input, $i, 1);
+    echo $output; //.menampilkan output karakter yang sudah dibalik
+}
+?>
