55a3de439af7efa0056c6302f72e135637328db2
kitapunyarasa <34958410+kitapunyarasa@users.noreply.github.com>
2018-02-13 14:09:57 +0700

    update test.php
    cobain

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$input = "test123"; //.masukkan kata atau kalimat yang ingin dibalik
+$input = "test1231"; //.masukkan kata atau kalimat yang ingin dibalik
 //.mulai memproses input
 $count = strlen($input); //.menghitung jumlah karakter
 echo "<b>input = </b>" . $input; //.menampilkan input
